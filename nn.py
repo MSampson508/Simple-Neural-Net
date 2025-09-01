0165e041d6d14f613f7e45ee86ad132ab3767c1f
MSampson508 <sampsonmds@gmail.com>
2025-09-01 09:01:49 -0400

    still removing pyplot

--- nn.py
+++ nn.py
@@ -1,6 +1,5 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from matplotlib import pyplot as plt
 import json
 
 def forwardPropTest(w1, w2, b1, b2, Xi):
